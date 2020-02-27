de256ab266639d44dadbe6aebc356a779e689e32
kjschwoerke <kjschwoerke@yahoo.com>
2020-02-27 17:00:10 -0600

    initial push from desktop

---
+++ src/components/someComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Test = () => {
+    return (
+        <div className = "testDiv">
+            <h1>This is another test to add some more stuff!</h1>
+        </div>
+    )
+}
+
+export default Test
